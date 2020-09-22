bfc94a8aa729c921dc9739ef9b4dad08b42e4214
Ryan <ryannadiputraa@gmail.com>
2020-09-22 19:42:34 +0800

    train and saved the best model, plotting data using pyplot

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+from matplotlib import style
+import pickle
 
 data = pd.read_csv('student-mat.csv', sep=';')
 data = data[['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']]
@@ -13,16 +16,30 @@ predict = 'G3'
 
 X = np.array(data.drop([predict], 1))
 Y = np.array(data[predict])
-
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+'''
+best = 0
+for _ in range(100):  
+  x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.1)
+  linear = linear_model.LinearRegression()
 
-linear = linear_model.LinearRegression()
+  # find the fit line (train model)
+  linear.fit(x_train, y_train)
+  # test model accuracy
+  acc = linear.score(x_test, y_test)    
+  print('Accuracy : ', acc)
 
-# find the fit line (train model)
-linear.fit(x_train, y_train)
-# test model accuracy
-acc = linear.score(x_test, y_test)    
-print('Accuracy : ', acc)
+
+  # save the model
+  if acc > best:
+    best = acc
+    with open('studentmodel.pickle', 'wb') as f:
+      pickle.dump(linear, f)'''
+
+#open and load the model
+pickle_in = open('studentmodel.pickle', 'rb')
+linear = pickle.load(pickle_in)
+
 
 # Coefficient or Slope (m), in this case its multidimensional
 print('Coefficient : \n', linear.coef_)
@@ -34,3 +51,13 @@ predictions = linear.predict(x_test)
   
 for x in range(len(predictions)):
   print(predictions[x], x_test[x], y_test[x])
+
+
+# plotting
+second_grade = 'absences'
+third_grade = 'G3'
+style.use('ggplot')
+pyplot.scatter(data[second_grade], data[third_grade])
+pyplot.xlabel('Second Grade')
+pyplot.ylabel('Third Grade')
+pyplot.show()
